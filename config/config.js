43f4fbdda59e794cfc6a94434c06669eb005ea51
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-02-13 10:04:36 +0530

    Add production configuration in config file

--- config/config.js
+++ config/config.js
@@ -6,5 +6,18 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
+  },
+  "production": {
+    "username": process.env.AWS_USERNAME,
+    "password": process.env.AWS_PASSWORD,
+    "database": "postgres",
+    "host": process.env.AWS_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
