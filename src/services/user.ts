a2eb3c74421297ba4df861c15800664583b819c6
Shaik Mahammad Rafi <rafi.shaik@everest.engineering>
2026-02-23 23:18:41 +0530

    Add user service function to post a new user and sns service

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,26 @@
+import { sendEmail } from "../../config/aws_sns_config";
 import { User } from "../../models/User";
 
 
 export const getUsers = async () => {
     return User.findAll();
 }
+
+export const createNewUser = async (name: string, email: string) => {
+    try {
+        const newUser = await User.create({ name, email });
+        await newUser.save();
+        if (newUser) {
+            try {
+                await sendEmail(await newUser.get());
+            } catch (Error) {
+                console.error(Error, "User saved, but SNS failed:",);
+            }
+            return newUser;
+        }
+        return false;
+    } catch (error) {
+        console.error("Service Error:", error);
+        throw error;
+    }
+};
